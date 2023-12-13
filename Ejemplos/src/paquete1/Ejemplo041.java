7893125e17bc73911a689315254dc26819a13393
Luis Cordova <lcordovacarrion@gmail.com>
2023-12-13 16:21:41 -0500

    Arreglos

--- Ejemplos/src/paquete1/Ejemplo041.java
+++ Ejemplos/src/paquete1/Ejemplo041.java
@@ -9,27 +9,32 @@ package paquete1;
  *
  * @author reroes
  */
-public class Ejemplo04 {
+public class Ejemplo041 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-        
+
         // arreglo de tipo entero
         // int[] arreglo1 = new int[5];
         int[] arreglo1 = {1000, 1, 2, 2000, 300};
+        String cadena = "";
+        int suma = 0;
         // System.out.println(arreglo1.length);
         // arreglo1[2] = 1000;
         for (int i = 0; i < arreglo1.length; i++) {
             // La palabra length me va a indicar la cantidad de elementos de
             // un arreglo asociado
-            System.out.printf("Subíndice o Índice %d, con valor %d\n",
-                    i, arreglo1[i]);
+            suma = suma + arreglo1[i];
+            cadena = String.format("%sSubíndice o Índice %d, con valor "
+                    + "%d\n", cadena, i, arreglo1[i]);
         }
-        
-        
+        cadena = String.format("%sLa suma de los elementos es %d\n",
+                cadena, suma);
+        cadena = cadena.toUpperCase();
+        System.out.printf("%s", cadena);
     }
 
 }
